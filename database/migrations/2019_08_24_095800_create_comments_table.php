585d9599967aab38766e731fd7644f69df92573f
marioashok4 <marioashok4@gmail.com>
2019-08-24 15:57:25 +0530

    Added One to Many Relationship

---
+++ database/migrations/2019_08_24_095800_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('content');
+            $table->unsignedBigInteger('blog_id')->index();
+            $table->foreign('blog_id')->references('id')->on('blogs');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
